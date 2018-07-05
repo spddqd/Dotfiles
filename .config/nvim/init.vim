739dd6c5c6d67ae2b2c7f40b171e709329148002
spddqd <spddqd@gmail.com>
2018-07-05 21:08:24 +0300

    config folder

---
+++ .config/nvim/init.vim
@@ -0,0 +1,9 @@
+call plug#begin()
+Plug 'SirVer/ultisnips' | Plug 'honza/vim-snippets'
+Plug 'myusuf3/numbers.vim' | Plug 'Yggdroot/indentLine'
+Plug 'vim-airline/vim-airline' | Plug 'vim-airline/vim-airline-themes'
+Plug 'dylanaraps/wal.vim'
+call plug#end()
+
+let g:airline_theme='bubblegum'
+colorscheme wal
---
+++ .config/polybar/launch.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#!/usr/bin/env sh
+
+# Set wallpaper and colors in Xresources
+wal -i ~/Pictures/bg.png
+
+# Terminate already running bar instances
+killall -q polybar
+
+# Wait until the processes have been shut down
+while pgrep -x polybar >/dev/null; do sleep 1; done
+
+for i in $(polybar -m | awk -F: '{print $1}'); do MONITOR=$i polybar bar -c ~/.config/polybar/config & done
